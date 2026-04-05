55e0c769b8561375bce0f8b6aacfb35133f54a29
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-04-05 09:34:51 -0400

    feat: Agrega bloque try catch para la inicializacion de la BD

--- src/main/java/org/proyectobdmotos/App.java
+++ src/main/java/org/proyectobdmotos/App.java
@@ -1,11 +1,25 @@
 package org.proyectobdmotos;
 
+import java.sql.SQLException;
+
+import org.proyectobdmotos.controller.Agencia;
 import org.proyectobdmotos.database.DatabaseConnection;
 
 public class App {
 
     public static void main(String[] args) {
-        DatabaseConnection.runMigrations();
-        System.out.println("Base de datos lista.");
+    
+    try {
+      DatabaseConnection.runMigrations();
+      System.out.println("Base de datos lista.");
+
+      Agencia agencia = new Agencia(DatabaseConnection.getInstance());
+      agencia.cargarDatos();
+      System.out.println("Datos cargados en memoria.");
+      
+    } catch (SQLException e) {
+        e.printStackTrace();
+        System.err.println(e.getMessage() + "Error al inicializar la base de datos ");
     }
+  }
 }
