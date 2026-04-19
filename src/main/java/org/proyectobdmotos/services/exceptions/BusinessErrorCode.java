6ee87fbfd5dd9b99b0d3f8df93cac06b8c8f541c
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-04-19 15:58:01 -0400

    refactor(services): align contrato flows with business error codes

--- src/main/java/org/proyectobdmotos/services/exceptions/BusinessErrorCode.java
+++ src/main/java/org/proyectobdmotos/services/exceptions/BusinessErrorCode.java
@@ -2,6 +2,9 @@ package org.proyectobdmotos.services.exceptions;
 
 public enum BusinessErrorCode {
     CLIENTE_NO_ENCONTRADO,
+    MOTO_NO_ENCONTRADA,
     MOTO_NO_DISPONIBLE,
+    CONTRATO_NO_ENCONTRADO,
+    CONTRATO_YA_FINALIZADO,
     CONTRATO_VALIDACION_FALLIDA
 }
