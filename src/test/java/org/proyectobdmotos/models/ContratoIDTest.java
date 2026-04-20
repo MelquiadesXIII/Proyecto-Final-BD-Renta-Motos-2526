a4e2267d11e91b01c65c3e2f60f7b0c770d1a289
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-04-20 07:57:58 -0400

    refactor(models): implement ContratoID equality contract with tests

---
+++ src/test/java/org/proyectobdmotos/models/ContratoIDTest.java
@@ -0,0 +1,69 @@
+package org.proyectobdmotos.models;
+
+import java.time.LocalDate;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ContratoIDTest extends TestCase {
+
+    public ContratoIDTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(ContratoIDTest.class);
+    }
+
+    public void testEqualsDebeRetornarTrueCuandoEsMismaReferencia() {
+        ContratoID contratoId = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+
+        assertTrue(contratoId.equals(contratoId));
+    }
+
+    public void testEqualsDebeRetornarFalseCuandoObjetoEsNull() {
+        ContratoID contratoId = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+
+        assertFalse(contratoId.equals(null));
+    }
+
+    public void testEqualsDebeRetornarFalseCuandoTipoEsDistinto() {
+        ContratoID contratoId = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+
+        assertFalse(contratoId.equals("no-es-contrato-id"));
+    }
+
+    public void testEqualsDebeCompararSoloCamposDeLlavePrimaria() {
+        ContratoID contratoIdA = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+        ContratoID contratoIdB = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+
+        assertTrue(contratoIdA.equals(contratoIdB));
+        assertTrue(contratoIdB.equals(contratoIdA));
+    }
+
+    public void testEqualsDebeSerNullSafeParaCamposDelId() {
+        ContratoID contratoIdConNulosA = new ContratoID(null, null);
+        ContratoID contratoIdConNulosB = new ContratoID(null, null);
+        ContratoID contratoIdConMatriculaDistinta = new ContratoID(null, "M-202");
+
+        assertTrue(contratoIdConNulosA.equals(contratoIdConNulosB));
+        assertFalse(contratoIdConNulosA.equals(contratoIdConMatriculaDistinta));
+    }
+
+    public void testHashCodeDebeSerConsistenteConEquals() {
+        ContratoID contratoIdA = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+        ContratoID contratoIdB = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+
+        assertTrue(contratoIdA.equals(contratoIdB));
+        assertEquals(contratoIdA.hashCode(), contratoIdB.hashCode());
+    }
+
+    public void testHashCodeDebeSerDeterminista() {
+        ContratoID contratoId = new ContratoID(LocalDate.of(2026, 4, 20), "M-101");
+        int primerHash = contratoId.hashCode();
+        int segundoHash = contratoId.hashCode();
+
+        assertEquals(primerHash, segundoHash);
+    }
+}
