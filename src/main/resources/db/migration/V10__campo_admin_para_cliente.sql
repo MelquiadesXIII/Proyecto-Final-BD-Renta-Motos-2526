011df4f5ee1d74adcb8469b3a50bc1f735709a6f
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-05-18 01:58:27 -0400

    Fead: Agregue nueva forma de saber si es admin un cliente

--- src/main/resources/db/migration/V10__campo_admin_para_cliente.sql
+++ src/main/resources/db/migration/V10__campo_admin_para_cliente.sql
@@ -1,9 +1,7 @@
-ALTER TABLE usuario ADD COLUMN es_admin BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE usuario ADD COLUMN IF NOT EXISTS es_admin BOOLEAN NOT NULL DEFAULT false;
 
-
-INSERT INTO usuario (nombre_usuario, password, gmail, es_admin)
-VALUES ('Dario_Admin', 'Admin123', 'admindario@motos.cu', true)
-VALUES ('DarelL_Admin', 'Admin123', 'admindarell@motos.cu', true)
-VALUES ('Lian_Admin', 'Admin123', 'adminlian@motos.cu', true)
+INSERT INTO usuario (nombre_usuario, password, gmail, es_admin) VALUES
+('Dario_Admin', 'Admin123', 'admindario@motos.cu', true),
+('DarelL_Admin', 'Admin123', 'admindarell@motos.cu', true),
+('Lian_Admin', 'Admin123', 'adminlian@motos.cu', true)
 ON CONFLICT (gmail) DO NOTHING;
-
