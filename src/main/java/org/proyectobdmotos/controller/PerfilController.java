c5f0d4f037974f23f3d6c195905ad82239578427
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-05-19 14:53:22 -0400

    Fead: COlocacion de la imagen en el PerfilCOntroler

--- src/main/java/org/proyectobdmotos/controller/PerfilController.java
+++ src/main/java/org/proyectobdmotos/controller/PerfilController.java
@@ -2,17 +2,30 @@ package org.proyectobdmotos.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import org.proyectobdmotos.models.Usuario;
 
 public class PerfilController {
 
     @FXML
-    private Label labelNombreUsuario;  // Asegúrate de que tu perfil.fxml tenga un Label con fx:id="labelNombreUsuario"
+    private Label labelNombreUsuario;
+    @FXML
+    private ImageView fondoPerfil;   // para el binding
 
     private Usuario usuarioActual;
 
-    public void setUsuario(Usuario usuario) 
-    {
+    @FXML
+    private void initialize() {
+        // Responsive background
+        if (fondoPerfil != null) {
+            StackPane parent = (StackPane) fondoPerfil.getParent();
+            fondoPerfil.fitWidthProperty().bind(parent.widthProperty());
+            fondoPerfil.fitHeightProperty().bind(parent.heightProperty());
+        }
+    }
+
+    public void setUsuario(Usuario usuario) {
         this.usuarioActual = usuario;
         if (labelNombreUsuario != null) {
             labelNombreUsuario.setText("Bienvenido, " + usuario.getNombreUsuario());
