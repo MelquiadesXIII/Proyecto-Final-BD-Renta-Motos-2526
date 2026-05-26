508fd2f9620ab62b1b9bb648e32a32bfb6df38e3
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-05-26 15:40:11 -0400

    Update: Colocacion de los nuevos Querrys en funciones

--- src/main/resources/db/migration/V11__agrupadores.sql
+++ src/main/resources/db/migration/V11__agrupadores.sql
@@ -1,7 +1,34 @@
-SELECT nombre_color FROM Color;
+-- Función que devuelve el nombre de todos los colores
+CREATE OR REPLACE FUNCTION obtener_colores()
+RETURNS TABLE (nombre_color VARCHAR)
+LANGUAGE SQL
+AS $$
+    SELECT nombre_color FROM Color;
+$$;
 
-SELECT matricula_moto, model.nombre_modelo, sit.nombre_situacion, col.nombre_color, cant_km_recorridos
-FROM Moto m
-JOIN color col ON m.id_color = col.id_color
-JOIN modelo model ON m.id_modelo = model.id_modelo
-JOIN situacion sit ON m.id_situacion = sit.id_situacion;
+-- SELECT * FROM obtener_colores();
+
+
+-- Funcion que devuelve los detalles de las motos
+CREATE OR REPLACE FUNCTION obtener_detalles_motos()
+RETURNS TABLE (
+    matricula_moto  VARCHAR,
+    nombre_modelo   VARCHAR,
+    nombre_situacion VARCHAR,
+    nombre_color    VARCHAR,
+    cant_km_recorridos INTEGER
+)
+LANGUAGE SQL
+AS $$
+    SELECT m.matricula_moto,
+           model.nombre_modelo,
+           sit.nombre_situacion,
+           col.nombre_color,
+           m.cant_km_recorridos
+    FROM Moto m
+    JOIN color col     ON m.id_color = col.id_color
+    JOIN modelo model  ON m.id_modelo = model.id_modelo
+    JOIN situacion sit ON m.id_situacion = sit.id_situacion;
+$$;
+
+-- SELECT * FROM obtener_detalles_motos();
