931424907ad9d882303a24fc5e56ebd3bfd0c59e
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-05-28 22:27:55 -0400

    Fead: Nuevas Funcionalidades en MotoDao

--- src/main/java/org/proyectobdmotos/dao/IMotoDAO.java
+++ src/main/java/org/proyectobdmotos/dao/IMotoDAO.java
@@ -1,22 +1,27 @@
 package org.proyectobdmotos.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.proyectobdmotos.dto.MotoDTO;
 import org.proyectobdmotos.dto.SituacionMotoDTO;
-import org.proyectobdmotos.models.Moto;
-import org.proyectobdmotos.models.Situacion;
+import org.proyectobdmotos.models.*;
 
 public interface IMotoDAO extends GenericDAO<Moto, Integer> {
 
     List<MotoDTO> listarMotosConKilometraje();
-
     List<SituacionMotoDTO> listarSituacionMotos();
-
     void cambiarEstado(Integer idMoto, Situacion nuevaSituacion);
-
     boolean estaDisponible(Integer idMoto);
-
     Optional<Moto> buscarPorMatricula(String matricula);
+
+    ArrayList<Color> obtenerColores() throws SQLException;
+    ArrayList<Marca> obtenerMarcas() throws SQLException;
+    ArrayList<Modelo> obtenerModelosPorMarca(int idMarca) throws SQLException;
+    Modelo obtenerModeloPorId(int idModelo) throws SQLException;
+    Marca obtenerMarcaPorId(int idMarca) throws SQLException;
+    int obtenerIdColorPorNombre(String nombreColor) throws SQLException;
+    String obtenerNombreColorPorId(int idColor) throws SQLException;
 }
