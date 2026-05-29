d988c6059055662223ffd95200ae8df80eed78ec
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-05-29 16:07:27 -0400

    Fix: Cambio de nombre de ID, para evitar problemas

--- src/main/java/org/proyectobdmotos/models/Usuario.java
+++ src/main/java/org/proyectobdmotos/models/Usuario.java
@@ -2,24 +2,25 @@ package org.proyectobdmotos.models;
 
 
 public class Usuario {
-    private Integer id;
+    private Integer id_usuario;
     private String nombreUsuario;
     private String password;   
     private String gmail;
     private boolean esAdmin;
 
-    public Usuario() {}
 
-    public Usuario(Integer id, String nombreUsuario, String password, String gmail, boolean esAdmin) {
-        this.id = id;
+    public Usuario(){}
+    
+    public Usuario(Integer id_usuario, String nombreUsuario, String password, String gmail, boolean esAdmin) {
+        this.id_usuario = id_usuario;
         this.nombreUsuario = nombreUsuario;
         this.password = password;
         this.gmail = gmail;
         this.esAdmin = esAdmin;
     }
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    public Integer getId() { return id_usuario; }
+    public void setId(Integer id) { this.id_usuario = id_usuario; }
     public String getNombreUsuario() { return nombreUsuario; }
     public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
     public String getPassword() { return password; }
