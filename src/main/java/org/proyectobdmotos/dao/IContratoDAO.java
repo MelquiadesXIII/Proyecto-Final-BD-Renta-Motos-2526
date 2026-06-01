432423bf4171cd03e2cc282e64fb14d234450e8f
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-01 00:48:17 -0400

    Fead: COlocacion de los metodos en la interfaz IContrato

--- src/main/java/org/proyectobdmotos/dao/IContratoDAO.java
+++ src/main/java/org/proyectobdmotos/dao/IContratoDAO.java
@@ -2,9 +2,21 @@ package org.proyectobdmotos.dao;
 
 import java.util.List;
 
+import org.proyectobdmotos.dto.ContRepDTO;
+import org.proyectobdmotos.dto.IngAnualDTO;
+import org.proyectobdmotos.dto.ResMarModDTO;
+import org.proyectobdmotos.dto.ResMunDTO;
 import org.proyectobdmotos.models.Contrato;
 
 public interface IContratoDAO extends GenericDAO<Contrato, Integer> {
 
     List<Contrato> listarContratosCompletos();
+    
+    List<ContRepDTO> listarContratosReporte();
+
+    List<ResMarModDTO> resumenMarcasModelos();
+
+    List<ResMunDTO> resumenMunicipios();
+
+    List<IngAnualDTO> ingresosAnuales();
 }
