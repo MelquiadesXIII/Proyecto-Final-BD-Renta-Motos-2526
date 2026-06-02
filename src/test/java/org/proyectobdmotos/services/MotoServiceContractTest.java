8cb927dd573e04b1cedb1eacd2549015f74220b6
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-01 23:04:57 -0400

    Update: Faltaba colocar unas librrerias mas...

--- src/test/java/org/proyectobdmotos/services/MotoServiceContractTest.java
+++ src/test/java/org/proyectobdmotos/services/MotoServiceContractTest.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.proyectobdmotos.dto.MotoRepDTO;
+import org.proyectobdmotos.dto.SitMotoRepDTO;
+import org.proyectobdmotos.models.Color;
+import org.proyectobdmotos.models.Marca;
+import org.proyectobdmotos.models.Modelo;
+import java.sql.SQLException;
 import org.proyectobdmotos.dao.IMotoDAO;
 import org.proyectobdmotos.dto.MotoDTO;
 import org.proyectobdmotos.dto.MotoRepDTO;
