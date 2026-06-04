184fbd537e66f823353810467e8c9abc72f3cd4b
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-04 18:47:23 -0400

    Fead: Creacion de LCiuenteUsuarioDTO
    Aqui lo que hice no se entendera hasta que suba lo nuevo que planie. Que es que le Admin pueda ver toda la informacion del usuario y asi evitar problemas

---
+++ src/main/java/org/proyectobdmotos/dto/ClienteUsuarioDTO.java
@@ -0,0 +1,35 @@
+package org.proyectobdmotos.dto;
+
+public class ClienteUsuarioDTO {
+    private final int idCliente;
+    private final int idUsuario;
+    private final String ci;
+    private final String nombreCompleto;
+    private final String telefono;
+    private final String nombreMunicipio;
+    private final String nombreUsuario;
+    private final String gmail;
+
+    public ClienteUsuarioDTO(int idCliente, int idUsuario, String ci,
+                             String nombreCompleto, String telefono,
+                             String nombreMunicipio, String nombreUsuario,
+                             String gmail) {
+        this.idCliente = idCliente;
+        this.idUsuario = idUsuario;
+        this.ci = ci;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.nombreMunicipio = nombreMunicipio;
+        this.nombreUsuario = nombreUsuario;
+        this.gmail = gmail;
+    }
+
+    public int getIdCliente() { return idCliente; }
+    public int getIdUsuario() { return idUsuario; }
+    public String getCi() { return ci; }
+    public String getNombreCompleto() { return nombreCompleto; }
+    public String getTelefono() { return telefono; }
+    public String getNombreMunicipio() { return nombreMunicipio; }
+    public String getNombreUsuario() { return nombreUsuario; }
+    public String getGmail() { return gmail; }
+}
