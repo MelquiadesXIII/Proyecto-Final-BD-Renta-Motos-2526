755e436e91981e04ad6e76d01e4d164189475fca
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-11 22:39:19 -0400

    Fead: Creacion de los atributos de ClienteActual, UsuarioACtual

--- src/main/java/org/proyectobdmotos/stores/AgenciaStore.java
+++ src/main/java/org/proyectobdmotos/stores/AgenciaStore.java
@@ -8,12 +8,17 @@ import org.proyectobdmotos.models.Moto;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.proyectobdmotos.models.Usuario;
 
 public final class AgenciaStore {
 
     private final ObservableList<Cliente> clientes;
     private final ObservableList<Moto> motos;
     private final ObservableList<Contrato> contratos;
+    private Cliente clienteActual;
+    private Usuario UsuarioActual;
+
+
 
     public AgenciaStore() {
         clientes = FXCollections.observableArrayList();
@@ -21,6 +26,7 @@ public final class AgenciaStore {
         motos = FXCollections.observableArrayList();
     }
 
+    // ============ Listas observables ============
     public ObservableList<Cliente> getClientes() {
         return clientes;
     }
@@ -34,12 +40,27 @@ public final class AgenciaStore {
     public void setClientes(Collection<Cliente> nuevos) {
         clientes.setAll(nuevos);
     }
-
     public void setMotos(Collection<Moto> nuevos) {
         motos.setAll(nuevos);
     }
-
     public void setContratos(Collection<Contrato> nuevos) {
         contratos.setAll(nuevos);
+    }
+
+    // ============ Cliente actual ============
+    public Cliente getClienteActual() {
+        return clienteActual;
+    }
+    public void setClienteActual(Cliente cliente) {
+        this.clienteActual = cliente;
+    }
+
+    // ============ Usuario actual ============
+    public Usuario getUsuarioActual() {
+        return UsuarioActual;
+    }
+
+    public void setUsuarioActual(Usuario usuarioActual) {
+        UsuarioActual = usuarioActual;
     }
 }
