5d8249d4a2f24389949eceadd71fa05f1305f83c
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-15 18:12:30 -0400

    Fead: Colocaion de una imagen en BienvenidoUsuario

--- src/main/java/org/proyectobdmotos/controller/BienvenidoUsuarioController.java
+++ src/main/java/org/proyectobdmotos/controller/BienvenidoUsuarioController.java
@@ -2,12 +2,15 @@ package org.proyectobdmotos.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import org.proyectobdmotos.models.Cliente;
 import org.proyectobdmotos.stores.AgenciaStore;
 
 public class BienvenidoUsuarioController {
 
     @FXML private Label labelBienvenida;
+    @FXML private StackPane rootPane;
+
     private final AgenciaStore agenciaStore;
 
     public BienvenidoUsuarioController(AgenciaStore agenciaStore) {
@@ -16,6 +19,17 @@ public class BienvenidoUsuarioController {
 
     @FXML
     private void initialize() {
+        if (rootPane != null) {
+            rootPane.setStyle(
+                    "-fx-background-image: url('"
+                            + getClass().getResource("/Utiles/intro_usuario.jpg").toExternalForm()
+                            + "');"
+                            + "-fx-background-size: cover;"
+                            + "-fx-background-position: center center;"
+                            + "-fx-background-repeat: no-repeat;"
+            );
+        }
+
         Cliente cliente = agenciaStore.getClienteActual();
         String nombre = (cliente != null) ? cliente.getNombreCliente() : "Usuario";
         labelBienvenida.setText("Bienvenido Usuario " + nombre);
