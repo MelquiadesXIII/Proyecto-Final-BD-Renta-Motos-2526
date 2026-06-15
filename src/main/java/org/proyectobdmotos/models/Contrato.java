b5fb1dd993b2b88361f8b93b2956a85fece1c18d
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-06-14 20:24:05 -0400

    Fead: Creacion d enuevos atributos y modifciaion de los calculos

--- src/main/java/org/proyectobdmotos/models/Contrato.java
+++ src/main/java/org/proyectobdmotos/models/Contrato.java
@@ -6,7 +6,6 @@ import java.time.temporal.ChronoUnit;
 import org.proyectobdmotos.utils.Validator;
 
 @SuppressWarnings("OverridableMethodCallDuringObjectConstruction")
-
 public class Contrato {
 
     private Integer idContrato;
@@ -23,10 +22,16 @@ public class Contrato {
     private double cantKmSalida;
     private double cantKmLlegada;
 
+    private String ciCliente;
+    private String nombreCompletoCliente;
+    private String matriculaMoto;
+    private String marcaMoto;
+    private String modeloMoto;
+
     public Contrato(double cantKmLlegada, double cantKmSalida, Integer idCliente, int diasProrroga,
-            LocalDate fechaEntrega, LocalDate fechaFin, LocalDate fechaInicio,
-            FormaPago formaPago, Integer idMoto, boolean seguroAdicional,
-            double tarifaNormal, double tarifaProrroga) {
+                    LocalDate fechaEntrega, LocalDate fechaFin, LocalDate fechaInicio,
+                    FormaPago formaPago, Integer idMoto, boolean seguroAdicional,
+                    double tarifaNormal, double tarifaProrroga) {
         setCantKmLlegada(cantKmLlegada);
         setCantKmSalida(cantKmSalida);
         setIdCliente(idCliente);
@@ -41,132 +46,87 @@ public class Contrato {
         setTarifaProrroga(tarifaProrroga);
     }
 
-    public Integer getIdContrato() {
-        return idContrato;
-    }
+    public Integer getIdContrato() { return idContrato; }
+    public void setIdContrato(Integer idContrato) { this.idContrato = idContrato; }
 
-    public void setIdContrato(Integer idContrato) {
-        this.idContrato = idContrato;
-    }
-
-    public LocalDate getFechaInicio() {
-        return fechaInicio;
-    }
-
+    public LocalDate getFechaInicio() { return fechaInicio; }
     public void setFechaInicio(LocalDate fechaInicio) {
         Validator.validateLocalDate(fechaInicio);
         this.fechaInicio = fechaInicio;
     }
 
-    public Integer getIdMoto() {
-        return idMoto;
-    }
-
+    public Integer getIdMoto() { return idMoto; }
     public void setIdMoto(Integer idMoto) {
         Validator.nonNull(idMoto);
         this.idMoto = idMoto;
     }
 
-    public LocalDate getFechaFin() {
-        return fechaFin;
-    }
-
+    public LocalDate getFechaFin() { return fechaFin; }
     public void setFechaFin(LocalDate fechaFin) {
         Validator.validateLocalDate(fechaFin);
         this.fechaFin = fechaFin;
     }
 
-    public Integer getIdCliente() {
-        return idCliente;
-    }
-
+    public Integer getIdCliente() { return idCliente; }
     public void setIdCliente(Integer idCliente) {
         Validator.nonNull(idCliente);
         this.idCliente = idCliente;
     }
 
-    public FormaPago getFormaPago() {
-        return formaPago;
-    }
-
+    public FormaPago getFormaPago() { return formaPago; }
     public void setFormaPago(FormaPago formaPago) {
         Validator.nonNull(formaPago);
         this.formaPago = formaPago;
     }
 
-    public int getDiasProrroga() {
-        return diasProrroga;
-    }
-
+    public int getDiasProrroga() { return diasProrroga; }
     public void setDiasProrroga(int diasProrroga) {
         boolean valid = diasProrroga >= 0;
         if (!valid) {
             throw new IllegalArgumentException(
-                "Los días de prórroga no pueden ser negativos. Recibido: " + diasProrroga);
+                    "Los días de prórroga no pueden ser negativos. Recibido: " + diasProrroga);
         }
         this.diasProrroga = diasProrroga;
     }
 
-    public boolean isSeguroAdicional() {
-        return seguroAdicional;
-    }
+    public boolean isSeguroAdicional() { return seguroAdicional; }
+    public void setSeguroAdicional(boolean seguroAdicional) { this.seguroAdicional = seguroAdicional; }
 
-    public void setSeguroAdicional(boolean seguroAdicional) {
-        this.seguroAdicional = seguroAdicional;
-    }
-
-    public double getTarifaNormal() {
-        return tarifaNormal;
-    }
-
+    public double getTarifaNormal() { return tarifaNormal; }
     public void setTarifaNormal(double tarifaNormal) {
         Validator.validatePositive(tarifaNormal);
         this.tarifaNormal = tarifaNormal;
     }
 
-    public double getTarifaProrroga() {
-        return tarifaProrroga;
-    }
-
+    public double getTarifaProrroga() { return tarifaProrroga; }
     public void setTarifaProrroga(double tarifaProrroga) {
         boolean valid = tarifaProrroga >= 0;
         if (!valid) {
             throw new IllegalArgumentException(
-                "La tarifa de prórroga no puede ser negativa. Recibida: " + tarifaProrroga);
+                    "La tarifa de prórroga no puede ser negativa. Recibida: " + tarifaProrroga);
         }
         this.tarifaProrroga = tarifaProrroga;
     }
 
-    public LocalDate getFechaEntrega() {
-        return fechaEntrega;
-    }
+    public LocalDate getFechaEntrega() { return fechaEntrega; }
+    public void setFechaEntrega(LocalDate fechaEntrega) { this.fechaEntrega = fechaEntrega; }
 
-    public void setFechaEntrega(LocalDate fechaEntrega) {
-        this.fechaEntrega = fechaEntrega;
-    }
-
-    public double getCantKmSalida() {
-        return cantKmSalida;
-    }
-
+    public double getCantKmSalida() { return cantKmSalida; }
     public void setCantKmSalida(double cantKmSalida) {
         boolean valid = cantKmSalida >= 0;
         if (!valid) {
             throw new IllegalArgumentException(
-                "Los km de salida no pueden ser negativos. Recibidos: " + cantKmSalida);
+                    "Los km de salida no pueden ser negativos. Recibidos: " + cantKmSalida);
         }
         this.cantKmSalida = cantKmSalida;
     }
 
-    public double getCantKmLlegada() {
-        return cantKmLlegada;
-    }
-
+    public double getCantKmLlegada() { return cantKmLlegada; }
     public void setCantKmLlegada(double cantKmLlegada) {
         boolean valid = cantKmLlegada >= 0;
         if (!valid) {
             throw new IllegalArgumentException(
-                "Los km de llegada no pueden ser negativos. Recibidos: " + cantKmLlegada);
+                    "Los km de llegada no pueden ser negativos. Recibidos: " + cantKmLlegada);
         }
         this.cantKmLlegada = cantKmLlegada;
     }
@@ -174,11 +134,9 @@ public class Contrato {
     public int calcularDiasPactados() {
         int diasPactados = 0;
         boolean tieneFechasValidas = false;
-
         if (fechaInicio != null && fechaFin != null && !fechaFin.isBefore(fechaInicio)) {
             tieneFechasValidas = true;
         }
-
         if (tieneFechasValidas) {
             long diferenciaDias = ChronoUnit.DAYS.between(fechaInicio, fechaFin);
             if (diferenciaDias == 0) {
@@ -187,48 +145,56 @@ public class Contrato {
                 diasPactados = (int) diferenciaDias;
             }
         }
-
         return diasPactados;
-    }
-
-    public int calcularDiasProrrogaReal() {
-        int diasProrrogaReal = 0;
-        boolean tieneFechaEntrega = false;
-
-        if (fechaEntrega != null) {
-            tieneFechaEntrega = true;
-        }
-
-        if (fechaFin != null && tieneFechaEntrega && fechaEntrega.isAfter(fechaFin)) {
-            diasProrrogaReal = (int) ChronoUnit.DAYS.between(fechaFin, fechaEntrega);
-        }
-
-        return diasProrrogaReal;
     }
 
     public double calcularImporteBase() {
         int diasPactados = calcularDiasPactados();
         double importeBase = 0.0;
-
         if (diasPactados > 0) {
-            importeBase = diasPactados * getTarifaNormal();
+            double tarifa = getTarifaNormal();
+            if (isSeguroAdicional()) { tarifa = tarifa * 2; }
+            importeBase = diasPactados * tarifa;
         }
-
         return importeBase;
+    }
+
+    public int calcularDiasProrrogaReal() {
+        int diasProrrogaReal = 0;
+        if (fechaEntrega != null && fechaFin != null && fechaEntrega.isAfter(fechaFin)) {
+            diasProrrogaReal = (int) ChronoUnit.DAYS.between(fechaFin, fechaEntrega);
+        }
+        return diasProrrogaReal;
     }
 
     public double calcularRecargoProrroga() {
         int diasProrrogaReal = calcularDiasProrrogaReal();
         double recargoProrroga = 0.0;
-
         if (diasProrrogaReal > 0) {
-            recargoProrroga = diasProrrogaReal * getTarifaProrroga();
+            double tarifa = getTarifaProrroga();
+            if (isSeguroAdicional()) { tarifa = tarifa * 2; }
+            recargoProrroga = diasProrrogaReal * tarifa;
         }
-
         return recargoProrroga;
     }
 
     public double calcularImporteTotalTeorico() {
         return calcularImporteBase() + calcularRecargoProrroga();
     }
+
+
+    public String getCiCliente() { return ciCliente; }
+    public void setCiCliente(String ciCliente) { this.ciCliente = ciCliente; }
+
+    public String getNombreCompletoCliente() { return nombreCompletoCliente; }
+    public void setNombreCompletoCliente(String nombreCompletoCliente) { this.nombreCompletoCliente = nombreCompletoCliente; }
+
+    public String getMatriculaMoto() { return matriculaMoto; }
+    public void setMatriculaMoto(String matriculaMoto) { this.matriculaMoto = matriculaMoto; }
+
+    public String getMarcaMoto() { return marcaMoto; }
+    public void setMarcaMoto(String marcaMoto) { this.marcaMoto = marcaMoto; }
+
+    public String getModeloMoto() { return modeloMoto; }
+    public void setModeloMoto(String modeloMoto) { this.modeloMoto = modeloMoto; }
 }
